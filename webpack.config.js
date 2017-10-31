1bf1274036414edc4ab2ea5627c612e7d6d071ab
Stefan <stadolf@gmail.com>
2017-10-31 22:55:19 +0100

    a working config for dev/prod

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,12 @@ const Encore = require('@symfony/webpack-encore');
 Encore
     .setOutputPath('web/dist/')
     .setPublicPath('/dist')
+    .setManifestKeyPrefix('')
     .cleanupOutputBeforeBuild()
-    .addEntry('index', './app/Resources/public/js/index.js')
-    .addStyleEntry('main', './app/Resources/public/scss/main.scss')
     .enableSassLoader()
+    .addEntry('index', './app/Resources/public/js/index.js')
+    //.addStyleEntry('main', './app/Resources/public/scss/main.scss')
+
     .enableSourceMaps(!Encore.isProduction());
 
 module.exports = Encore.getWebpackConfig();
