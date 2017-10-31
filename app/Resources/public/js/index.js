730e6b5759a17990a61ee80c819224659ce66006
Stefan <stadolf@gmail.com>
2017-10-31 18:09:07 +0100

    using webpack dev server for assets, symfony asset helper splitted configuration added babel compiliation symfony integration added global cash-dom $ for jquery simulation :)

--- app/Resources/public/js/index.js
+++ app/Resources/public/js/index.js
@@ -1,15 +1,19 @@
 import _ from 'lodash';
 
-import 'bulma/css/bulma.css';
-import 'font-awesome/css/font-awesome.css';
-import '../scss/simple.css';
+import '../scss/main.scss';
 
 function component() {
     const helloWorld = _.join(['Bulma', 'CSS', 'Framework'], ' ');
-    const elem = document.querySelector('section p.subtitle .framework');
-
-    elem.innerHTML = helloWorld;
+    $('section p.subtitle .framework').html(helloWorld);
 }
 
-component();
+$(() => {
+    component();
+})
 
+
+if (module.hot) {
+      module.hot.accept('./index.js', function() {
+           console.log('Accepting the updated index module!');
+        })
+}
