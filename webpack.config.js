3f2bc49fd08fb70c20c2222f2a51bb8d5360f1e2
Stefan A <stefan.adolf@m-cube.de>
2017-11-01 14:24:55 +0100

    added versioning, added dev-server npm command

--- webpack.config.js
+++ webpack.config.js
@@ -16,14 +16,10 @@ Encore
         'window.jQuery': 'cash-dom',
     })
     .addEntry('index', './app/Resources/public/js/index.js')
-    //.addStyleEntry('main', './app/Resources/public/scss/main.scss')
-
     .enableSourceMaps(!Encore.isProduction())
     .configureBabel(babelConfig => {
         babelConfig.presets.push('es2017');
-
-        // no plugins are added by default, but you can add some
-        // babelConfig.plugins.push('styled-jsx/babel');
     })
+    .enableVersioning(Encore.isProduction())
 
 module.exports = Encore.getWebpackConfig();
