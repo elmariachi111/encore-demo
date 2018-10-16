e0f6df2a2c162ffaa9a94108439fe432f5dfb739
Stefan <stadolf@gmail.com>
2018-10-16 23:04:02 +0200

    upgraded to webpack 4 slightly more markup

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,14 @@
 import _ from "lodash";
-
 import "bulma/css/bulma.css";
 import "font-awesome/css/font-awesome.css";
+
 import "../scss/simple.css";
 
 function component() {
-  const helloWorld = _.join(["Bulma", "CSS", "Framework"], " ");
+  const helloWorld = _.join(["Bulma.io", "CSS", "Framework"], " ");
   const elem = document.querySelector("section p.subtitle .framework");
 
   elem.innerHTML = helloWorld;
 }
+
 window.setTimeout(component, 2000);
