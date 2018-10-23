7c67e446e8979285888e480fa166a8cab6bc9233
Stefan A <stadolf@gmail.com>
2018-10-24 00:56:50 +0200

    added a single file vue component -> makes HMR work.

--- assets/js/index-vue.js
+++ assets/js/index-vue.js
@@ -1,25 +1,8 @@
 import Vue from "vue";
-
-Vue.component("like-button", {
-  data: () => ({
-    liked: false
-  }),
-
-  template: `<button class="button is-large is-fullwidth is-rounded"
-                     :class="{ 'is-warning' : liked, 'is-info': !liked }"
-                     @click="liked = !liked">
-    <div v-if="liked">
-        <i class="fa fa-heart" />
-        You like me
-    </div>
-    <div v-if="!liked">
-        <i class="fa fa-frown-o" />
-        no ones likes me
-    </div>
-    </button>
-    `
-});
+import LikeButton from "./vue-components/LikeButton";
 
 const app = new Vue({
-  el: "#vue-entry"
+  el: "#vue-entry",
+  template: "<LikeButton />",
+  components: { LikeButton }
 });
---
+++ assets/js/vue-components/LikeButton.vue
@@ -0,0 +1,26 @@
+<template>
+    <button class="button is-large is-fullwidth is-rounded"
+                     :class="{ 'is-warning' : liked, 'is-info': !liked }"
+                     @click="liked = !liked">
+    <div v-if="liked">
+        <i class="fa fa-heart" />
+        You like me
+    </div>
+    <div v-if="!liked">
+        <i class="fa fa-frown-o" />
+        no ones likes me.
+    </div>
+    </button>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    liked: false
+  })
+};
+</script>
+
+<style scoped>
+</style>
+
